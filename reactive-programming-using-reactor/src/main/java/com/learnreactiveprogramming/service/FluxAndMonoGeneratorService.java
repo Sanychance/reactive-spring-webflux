b00a3d0db91dad253c896196b8b39f3c15a16606
Sanaz Bonakdar <sbonakdar@deloitte.com>
2025-04-16 18:47:08 -0700

    Add Mon-Flux

--- reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
+++ reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
@@ -1,4 +1,148 @@
 package com.learnreactiveprogramming.service;
 
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.time.Duration;
+import java.util.List;
+import java.util.Random;
+
 public class FluxAndMonoGeneratorService {
+
+    public static void main(String[] args) {
+        FluxAndMonoGeneratorService fAm = new FluxAndMonoGeneratorService();
+        fAm.fluxNames()
+                .subscribe(names -> System.out.println("Names:" + names));
+        //using subscribe to access to the Flux - the elements in the flux will return in form of streams
+        //no dta will send from Flux until we call subscribe - subscribe is always attach to a publisher
+        //here the fluxName is the publisher and subscriber is attached to it
+
+        fAm.fluxNamesMap().subscribe(names -> System.out.println("Names_Map:"+names));
+
+        fAm.monoName("Sanaz").subscribe(name -> System.out.println("Mono Name is:"+name));
+    }
+
+    public Flux<String> fluxNames(){
+
+        return Flux.fromIterable(List.of("Sanaz","Soheil"))
+                .log(); //it will log each and every event that happens between the subscriber and publisher
+    }
+
+    //reactive streams are immutable so the result wont change
+    public Flux<String> fluxNames_Immutability(){
+
+      Flux<String> namesFlux =  Flux.fromIterable(List.of("Sanaz","Soheil"));
+      namesFlux.map(String::toUpperCase);
+      return  namesFlux;
+
+    }
+
+    /***************** MAP ********************
+     - use for 1 to 1 transformation
+     - Simple Transformation from T to V
+     - Simple for synchronous transformation
+     - Doesnt support transformations that returns Publisher
+
+     **/
+    public Flux<String> fluxNamesMap(){
+
+        return Flux.fromIterable(List.of("Sanaz","Soheil"))
+                //.map(name-> name.toUpperCase())
+                .map(String::toUpperCase)
+                .log(); //it will log each and every event that happens between the subscriber and publisher
+    }
+
+    //make the string uppercase and then return the ones that has
+    // the lenght > stringLenght and map to be like 4-SANAZ, 6-SOHEIL
+    public Flux<String> fluxNamesMap_Filter(int stringLenght){
+
+        //the whole chain of multiple functions below called pipeline iun Functional programming
+        return Flux.fromIterable(List.of("Sanaz","Soheil","Mehrdad"))
+                //.map(name-> name.toUpperCase())
+                .map(String::toUpperCase)
+                .filter(name -> name.length() > stringLenght)
+                .map(s ->s.length() +"-"+s)
+                .log(); //it will log each and every event that happens between the subscriber and publisher
+    }
+
+    /***************** Flat MAP **********************
+     - use for 1 to N transformation
+     - Its more than transformations, subscribes to Flux or Mono thats part of the transformations
+      and flattens it and sends it downstream
+     - Use for asynchronous transformation
+     - support transformations that returns Publisher
+
+     **/
+    public Flux<String> fluxNames_FlatMap(int stringLenght){
+
+        return Flux.fromIterable(List.of("Sanaz","Soheil","Mehrdad"))
+                .map(String::toUpperCase)
+                .filter(name -> name.length() > stringLenght)
+                //SOHEIL, MEHRDAD -> S,O,H,E,I,L,M,E,H,R,D,A,D
+                .flatMap(name -> splitName(name))
+                .log(); //it will log each and every event that happens between the subscriber and publisher
+    }
+
+    //when we are having a use case where ordering matters then we shouldnt use flatMap as it will call async
+    public Flux<String> fluxNames_FlatMapAsync(int stringLenght){
+
+        return Flux.fromIterable(List.of("Sanaz","Soheil","Mehrdad"))
+                .map(String::toUpperCase)
+                .filter(name -> name.length() > stringLenght)
+                //SOHEIL, MEHRDAD -> S,O,H,E,I,L,M,E,H,R,D,A,D
+                .flatMap(name -> splitName_WithDelay(name))
+                .log(); //it will log each and every event that happens between the subscriber and publisher
+    }
+
+    // if we need ordering then we should use concat Map but its much slower than flatMap
+    public Flux<String> fluxNames_concatMapAsync(int stringLenght){
+
+        return Flux.fromIterable(List.of("Sanaz","Soheil","Mehrdad"))
+                .map(String::toUpperCase)
+                .filter(name -> name.length() > stringLenght)
+                //SOHEIL, MEHRDAD -> S,O,H,E,I,L,M,E,H,R,D,A,D
+                .concatMap(name -> splitName_WithDelay(name))
+                .log(); //it will log each and every event that happens between the subscriber and publisher
+    }
+
+    //SOHEIL -> Flux(S,O,H,E,I,L)
+    public Flux<String> splitName_WithDelay(String name){
+        var charArray =  name.split("");
+        var delay = 1000; //new Random().nextInt(1000);
+        return Flux.fromArray(charArray)
+                .delayElements(Duration.ofMillis(delay));
+    }
+
+    //SOHEIL -> Flux(S,O,H,E,I,L)
+    public Flux<String> splitName(String name){
+       var charArray =  name.split("");
+       return Flux.fromArray(charArray);
+    }
+
+    public Mono<String> monoName(String name){
+        return Mono.just(name)
+                .log();
+    }
+
+    public Mono<String> monoName_Map_Filter(String name, int stringLenght){
+        return Mono.just(name)
+                .map(String::toUpperCase)
+                .filter(s-> s.length()>stringLenght)
+                .log();
+    }
+    // Use when transformation returns another Mono of some type. flatmap with return Mono<T>
+    // when we use RESP API call which requires Async calls
+    public Mono<List<String>> monoName_Map_Filter_FlatMap(String name, int stringLenght){
+        return Mono.just(name)
+                .map(String::toUpperCase)
+                .filter(s-> s.length()>stringLenght)
+                //SOHEIL, MEHRDAD -> S,O,H,E,I,L,M,E,H,R,D,A,D
+                .flatMap(this::splitStringToList)//Mono<List of S,A,N,Z
+                .log();
+    }
+
+    private Mono<List<String>> splitStringToList(String s) {
+        var charArray = s.split("");
+       return Mono.just(List.of(charArray));
+    }
 }
--- reactive-programming-using-reactor/src/test/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorServiceTest.java
+++ reactive-programming-using-reactor/src/test/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorServiceTest.java
@@ -1,7 +1,117 @@
 package com.learnreactiveprogramming.service;
 
+import org.junit.jupiter.api.Test;
+import reactor.test.StepVerifier;
+
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class FluxAndMonoGeneratorServiceTest {
+    FluxAndMonoGeneratorService fluxAndMonoGeneratorService = new FluxAndMonoGeneratorService();
 
+    @Test
+    void fluxNamesTest() {
+        //when
+        var fluxNames = fluxAndMonoGeneratorService.fluxNames();
+
+        //Then
+        StepVerifier.create(fluxNames)
+                //.expectNext("Sanaz", "Soheil")
+                .expectNextCount(2)
+                .verifyComplete();
+    }
+
+
+    @Test
+    void fluxNamesTest_Mixed_NextCount() {
+        //when
+        var fluxNames = fluxAndMonoGeneratorService.fluxNames();
+
+        //Then
+        StepVerifier.create(fluxNames) // create function will invoke the Subscribe internally which automatically triggers the publisher to send events
+                .expectNext("Sanaz")//check the next element if its sanaz
+                .expectNextCount(1)//check to see if the remaining item is 1 item
+                .verifyComplete();
+    }
+
+    @Test
+    void fluxNamesMapTest() {
+        //when
+        var fluxNames = fluxAndMonoGeneratorService.fluxNamesMap();
+
+        //Then
+        StepVerifier.create(fluxNames)
+                .expectNext("SANAZ", "SOHEIL")
+                .verifyComplete();
+    }
+
+    @Test
+    void fluxNames_Immutability() {
+        //when
+        var fluxNames = fluxAndMonoGeneratorService.fluxNames_Immutability();
+        //Then
+        StepVerifier.create(fluxNames)
+                .expectNext("Sanaz", "Soheil")
+                .verifyComplete();
+    }
+
+    @Test
+    void fluxNamesMap_Filter() {
+        //when
+        var fluxNames = fluxAndMonoGeneratorService.fluxNamesMap_Filter(5);
+
+        //Then
+        StepVerifier.create(fluxNames)
+                .expectNext("6-SOHEIL")
+                .verifyComplete();
+
+    }
+
+    @Test
+    void fluxNames_FlatMap() {
+        //when
+        var fluxNames = fluxAndMonoGeneratorService.fluxNames_FlatMap(5);
+
+        //Then
+        StepVerifier.create(fluxNames)
+                .expectNext("S","O","H","E","I","L","M","E","H","R","D","A","D")
+                .verifyComplete();
+    }
+
+    @Test
+    void fluxNames_FlatMapAsync() {
+        //when
+        var fluxNames = fluxAndMonoGeneratorService.fluxNames_FlatMapAsync(5);
+
+        //Then
+        StepVerifier.create(fluxNames)
+                //expectNext("S","O","H","E","I","L","M","E","H","R","D","A","D")
+                .expectNextCount(13)
+                .verifyComplete();
+    }
+
+    @Test
+    void fluxNames_concatMapAsync() {
+        //when
+        var fluxNames = fluxAndMonoGeneratorService.fluxNames_concatMapAsync(5);
+
+        //Then
+        StepVerifier.create(fluxNames)
+                .expectNext("S","O","H","E","I","L","M","E","H","R","D","A","D")
+                .verifyComplete();
+    }
+
+    @Test
+    void monoName_Map_Filter_FlatMap() {
+
+        //when
+        var monoName = fluxAndMonoGeneratorService.monoName_Map_Filter_FlatMap("Sanaz",4);
+        //Then
+        StepVerifier.create(monoName)
+                .expectNext(List.of("S","A","N","A","Z"))
+                .verifyComplete();
+
+        //Then
+    }
 }
